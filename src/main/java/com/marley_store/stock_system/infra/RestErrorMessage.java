f7c1517b8bb3d0aa8f702bf9218ba50e9cc5ea6e
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-04-14 00:20:33 -0300

    feat: add RestErrorMessage class

---
+++ src/main/java/com/marley_store/stock_system/infra/RestErrorMessage.java
@@ -0,0 +1,14 @@
+package com.marley_store.stock_system.infra;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class RestErrorMessage {
+    private int status;
+    private String message;
+}
