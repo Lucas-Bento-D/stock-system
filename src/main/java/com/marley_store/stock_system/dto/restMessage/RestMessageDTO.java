c86ab2f1585fdf2c7d46c82a5ed0a7975d03bd34
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-04-27 12:36:33 -0300

    feat: now RestMessageDTO is a record

--- src/main/java/com/marley_store/stock_system/dto/restMessage/RestMessageDTO.java
+++ src/main/java/com/marley_store/stock_system/dto/restMessage/RestMessageDTO.java
@@ -1,13 +1,7 @@
 package com.marley_store.stock_system.dto.restMessage;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@Getter
-@Setter
-public class RestMessageDTO {
-    private int status;
-    private String message;
+public record RestMessageDTO(
+        int status,
+        String message
+) {
 }
