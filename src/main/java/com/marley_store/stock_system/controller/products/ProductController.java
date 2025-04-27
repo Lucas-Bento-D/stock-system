05833ee705ba98baf96374468292ba301c682be1
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-04-27 10:27:48 -0300

    feat: Create post route for product

--- src/main/java/com/marley_store/stock_system/controller/products/ProductController.java
+++ src/main/java/com/marley_store/stock_system/controller/products/ProductController.java
@@ -1,14 +1,31 @@
 package com.marley_store.stock_system.controller.products;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.marley_store.stock_system.dto.product.CreateProductDTO;
+import com.marley_store.stock_system.dto.restMessage.RestMessageDTO;
+import com.marley_store.stock_system.service.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/get")
     public String getProduct(){
         return "online";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<RestMessageDTO> createProduct(@RequestBody CreateProductDTO createProductDTO){
+
+        productService.createProduct(createProductDTO);
+
+        RestMessageDTO successMessage = new RestMessageDTO(HttpStatus.OK.value(), "Product created succesfully!");
+        return ResponseEntity.status(HttpStatus.OK).body(successMessage);
     }
 }
--- src/main/java/com/marley_store/stock_system/service/product/ProductService.java
+++ src/main/java/com/marley_store/stock_system/service/product/ProductService.java
@@ -1,7 +1,27 @@
 package com.marley_store.stock_system.service.product;
 
+import com.marley_store.stock_system.dto.product.CreateProductDTO;
+import com.marley_store.stock_system.model.product.Product;
+import com.marley_store.stock_system.repository.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void createProduct(CreateProductDTO createProductDTO){
+        Product product = Product.builder()
+                .name(createProductDTO.name())
+                .quantity(createProductDTO.quantity())
+                .costPrice(createProductDTO.costPrice())
+                .codeBar(createProductDTO.codeBar())
+                .sellingPrice(createProductDTO.sellingPrice())
+                .build();
+
+        productRepository.save(product);
+    }
+
 }
