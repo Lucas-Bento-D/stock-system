fb539d794e9de6bd9ececbce81fcfd4fe5db7697
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-04-27 22:32:50 -0300

    feat: findByCodeBar return Optional product now

--- src/main/java/com/marley_store/stock_system/repository/product/ProductRepository.java
+++ src/main/java/com/marley_store/stock_system/repository/product/ProductRepository.java
@@ -4,10 +4,11 @@ import com.marley_store.stock_system.model.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByName(String name);
-    List<Product> findByCodeBar(Long codeBar);
+    Optional<Product> findByCodeBar(Long codeBar);
     <S extends Product> S saveAndFlush(S entity);
     boolean existsByName(String name);
     boolean existsByCodeBar(Long codeBar);
